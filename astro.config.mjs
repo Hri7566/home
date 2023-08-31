b9f3103282f3aee6891191c6f52f86253207750b
Hri7566 <hri7566@gmail.com>
2023-08-31 04:11:47 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "static",
-  adapter: vercel()
+    integrations: [tailwind(), react()],
+    output: "server",
+    adapter: vercel()
 });
